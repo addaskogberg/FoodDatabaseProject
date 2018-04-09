c692427666ac3ceffd221c27cf4b25b128900503
addaskogberg <adda.skogberg@gmail.com>
2018-04-09 12:50:53 +0200

    nodemon

--- app.js
+++ app.js
@@ -6,11 +6,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('./config/mongoose.js')
-//const session = require('express-session')
+// const session = require('express-session')
 const app = express()
 const path = require('path')
-
-
 
 app.set('port', process.env.PORT || 3000)
 
@@ -20,15 +18,11 @@ mongoose.run().catch(error => {
   process.exit(1)
 })
 
-
 // Parse application encoding
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 // Serve static files.
 app.use(express.static(path.join(__dirname, 'public')))
-
-
 
 // body-Parser
 app.use(bodyParser.urlencoded({extended: true}))
--- react.js
+++ react.js
@@ -4,6 +4,9 @@ function HelloWorld (props) {
   return <div>Hello {props.name}</div>
 }
 
+console.log(HelloWorld)
 // user.map(ser =< <div>{user.name}</div>) loop outputs userlist
 // <button onClick={delete}>Delete>/button>  event on button
 // {isAdmin && <h1>Hit Admin</h1>}  conditional
+
+// <h1 color='red'>{HelloWorld}</h1>
