5331ab81bc54e91775decee6544f6df67d0a6571
addaskogberg <adda.skogberg@gmail.com>
2018-04-09 10:53:49 +0200

    conditional

--- react.js
+++ react.js
@@ -4,4 +4,6 @@ function HelloWorld (props) {
   return <div>Hello {props.name}</div>
 }
 
-// user.map(ser =< <div>{user.name}</div>) outputs userlist
+// user.map(ser =< <div>{user.name}</div>) loop outputs userlist
+// <button onClick={delete}>Delete>/button>  event on button
+// {isAdmin && <h1>Hit Admin</h1>}  conditional
