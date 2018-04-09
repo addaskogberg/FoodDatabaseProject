a7b4ebaa92138b36c09e0ad88f7a6db5d04f3602
addaskogberg <adda.skogberg@gmail.com>
2018-04-09 10:53:22 +0200

    adding loops and buttons in react

--- react.js
+++ react.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-function HelloWorld(props {
-  return <div>Hello {props.name}>/div>
-})
+function HelloWorld (props) {
+  return <div>Hello {props.name}</div>
+}
+
+// user.map(ser =< <div>{user.name}</div>) outputs userlist
