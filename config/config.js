ca8200e30bddf2f275c5f1d83e83df04782ad6c0
addaskogberg <adda.skogberg@gmail.com>
2018-05-21 10:45:04 +0200

    changes in gitignore

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db:'mongodb://admin:Bitching1@ds229008.mlab.com:29008/addaskogberg',
+ // db_dev: 'mongodb://localhost:27017/login_demo',
+  db_food: 'mongodb://localhost:27017/foodData',
+};
