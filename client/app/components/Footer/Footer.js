6e3558419db5cb467984a3d14634339d0f8a7740
addaskogberg <adda.skogberg@gmail.com>
2018-05-31 12:57:17 +0200

    adjusting the footer

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -6,9 +6,9 @@ const winter = '../assets/img/footerStone.png';
 const Footer = () => (
   <footer>
    
-  <div style={{color:'#bdb0c0', backgroundColor:'red', height:'80px', position: 'absolute ', bottom:'0', width: '100%' ,backgroundImage: "url(" + winter + ")"}}> 
-
-    <p>Fyll på här med innehåll för footer</p>  
+  <div style={{color:'#bdb0c0', height:'100px', position: 'absolute ', bottom:'0', width: '100%' ,backgroundImage: "url(" + winter + ")"}}> 
+    <p>Den här sidan är ett MERN projekt och ingår som en leverans i kursen 1dv430 på LinnéUniversitetet.</p>  
+    <p>Skapad av Adda Skogberg,  <a style={{color:'#bdb0c0'}} href="https://github.com/1dv430/as224wq-project">  Link to my GitHub repo</a> </p> 
  
    </div> 
   </footer>
