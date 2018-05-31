fafad275adddeaa902573d2610f5327e814326ec
addaskogberg <adda.skogberg@gmail.com>
2018-05-31 13:01:19 +0200

    header

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -6,7 +6,7 @@ const winter = '../assets/img/footerStone.png';
 const Header = () => (
   <header >
   <div style={{ width: '100%' ,backgroundImage: "url(" + winter + ")"}}>
-    <h1 className="headerh1" > Mat i balans </h1>
+    <h1 style={{marginTop: '30px'}} className="headerh1" > Mat i balans </h1>
     <br />
      
     <Link to="/">Hem</Link>
