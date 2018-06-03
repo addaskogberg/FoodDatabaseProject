6dd62eca77930c6d32075ceced522e84c05bd5b3
addaskogberg <adda.skogberg@gmail.com>
2018-06-03 15:24:02 +0200

    change link to git

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
    
   <div style={{color:'#bdb0c0', height:'100px', position: 'absolute ', width: '1200px' ,backgroundImage: "url(" + winter + ")", bottom: '0'}}> 
     <p>Den här sidan är ett MERN projekt och ingår som en leverans i kursen 1dv430 på LinnéUniversitetet.</p>  
-    <p>Skapad av Adda Skogberg,  <a style={{color:'#bdb0c0'}} href="https://github.com/1dv430/as224wq-project">  Link to my GitHub repo</a> </p> 
+    <p>Skapad av Adda Skogberg,  <a style={{color:'#bdb0c0'}} href="https://github.com/addaskogberg">  Link to my GitHub repo</a> </p> 
  
    </div> 
   </footer>
